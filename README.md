cf2a2a22fe700d84bc05b4f2c03dec65addf63dc
NadiaFortune <hello@nadiafortune.co.uk>
2024-01-07 15:34:04 +0000

    finalised application

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Password-Generator
+
+## **Description**
+
+I created a simple random password generator that would provide a randomly-generated password of between 8 and 128 characters.
+
+The desired outcome was to allow for the following to be take place:
+
+-> The user could choose the length of the password
+-> The user could choose to include lower case letters
+-> The user could choose to include upper case letters
+-> The user could choose to include special characters 
+-> The user could choose to include numbers
+
+Javascript functions were applied to produce the random password of the length chosen.
+
+The deployed page can be found here: https://nadiafortune.github.io/Console-Finances/
+
+A screenshot of the deployed page is below:
+![](images/Screenshot_2023-12-28_195959.png)
+
+
+## **Installation**
+
+The application runs within the window and is initiated when the user clicks the 'Generate Password' button.
+
+
+## **Usage**
+
+N/A
+
+## **Credits**
+
+The following resources were used to research the coding funcionality for the successful deployment in the console: YouTube, w3schools, and MDN.
+
+I did not use a tutor guidance session for this challenge.
+
+## **Licence**
+
+Please refer to the LICENCE in the repo.
+
+
--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 // Array of special characters to be included in password
+var choiceArr = []  
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
 
 // Array of numeric characters to be included in password
@@ -30,88 +9,65 @@ var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
 
-// Function to prompt user for password options
+var characterLength = 8
+
+/// Function to prompt user for password options
 function getPasswordOptions() {
+  choiceArr = []
+  characterLength = prompt("How many characters do you want your password to be? (Choose between 8 and 128)")
 
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("Please make sure length is a number between 8 and 128. Please try again")
+    return false
+  }
+
+  if(confirm("Would you like to use lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCasedCharacters);
+  }
+  if(confirm("Would you like to use uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCasedCharacters);
+}
+  if(confirm("Would you like to use special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacters);
+}
+  if(confirm("Would you like to use numbers in your password?")) {
+    choiceArr = choiceArr.concat(numericCharacters);
+}    
+    return true
 }
 
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-}
-
-// Function to generate password with user input
+// Function to generate password with user input and get random element from an array
 function generatePassword() {
-
-}
+    var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+    return password
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
+  var correctPrompts = getPasswordOptions();
+
+  if (correctPrompts) {
+    
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
+  }
 }
 
 // Add event listener to generate button
